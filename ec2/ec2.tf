1594f481e6191913c8301fa4902f2cc633ded5c5
ktharuni <kompallytharuni@gmail.com>
2024-11-05 11:00:10 +0530

    terraform

---
+++ ec2/ec2.tf
@@ -0,0 +1,20 @@
+resource "aws-security-group" "allow_ssh_terraform" {
+    name = "allow_ssh"
+    description = "Allow port number 22 for SSH access"
+
+    egress {
+           from_port        = 0
+           to_port          = 0
+           protocol         = "-1"
+           cidr_blocks      = ["0.0.0.0/0"]
+           ipv6_cidr_blocks = ["::/0"]
+    }
+    
+    ingress {
+           from_port        = 22
+           to_port          = 22
+           protocol         = "tcp"
+           cidr_blocks      = ["0.0.0.0/0"]
+           ipv6_cidr_blocks = ["::/0"]
+    }
+}
